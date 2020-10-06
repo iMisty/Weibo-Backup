b738e89e8d469444fb662936a2d18ab24900faf9
Kagura Koeri <ling-miya@hotmail.com>
2020-10-06 16:11:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
-// const http = require('http');
-// const fs = require('fs');
-// const request = require('request');
-// const path = require('path');
-// const config = require('./config');
-// const analyze = require('./analyze');
-
-// function start() {
-//   console.warn('start');
-//   request(config.url, function (err, res, body) {
-//     console.log(err);
-//     console.log('start1');
-//     if (!err && res) {
-//       console.log('start2');
-//       analyze.findImg(body, download);
-//     }
-//   });
-// }
-
-// function download(imgUrl, i) {
-//   let ext = imgUrl + '.';
-
-//   request({ uri: imgUrl }).pipe(
-//     fs.createWriteStream(path.join(config.imgDir, i + '.' + ext), {
-//       encoding: 'binary',
-//     })
-//   );
-
-//   console.log(i);
-// }
-// start();
-
 const request = require('request');
 const fs = require('fs');
 const cheerio = require('cheerio');
-request('http://api.kagura.design/77526951.html', function (error, response, body) {
+const config = require('./config');
+const url = config.url;
+
+request(url, function (error, response, body) {
   console.log('error:', error); // 错误优先
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   //   fs.writeFileSync('./qn.html',body,'utf8')
